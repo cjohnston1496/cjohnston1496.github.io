472176576e217328714868d6f31b9d290946158c
cgjohn <cgjohn@umich.edu>
2016-12-22 13:29:31 -0500

    adding dot files

--- bus.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('busApp', []);
-
-app.controller("BusCtrl", function($scope, $http) {
-	console.log("start");
-	$http.jsonp("http://mbus.doublemap.com/map/v2/buses")
-		.then(function(response) {
-			console.log(response);
-		});
-});
