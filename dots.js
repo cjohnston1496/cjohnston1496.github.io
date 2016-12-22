3fdca3d721ebd3961be2c8a0d5962829206be081
cjohnston1496 <cjohnston1496@gmail.com>
2016-12-22 13:25:00 -0500

    Add files via upload

---
+++ dots.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+
+    function goRight(){
+        document.images['ball1'].style.left = parseInt(document.images['ball1'].style.left)+5 +"px";
+    } 
+    
+
+    function updateDots() {
+        var dot, body, x, y;
+
+        if (Math.random() < 0.07) {
+            orangeDot = $('<img src="orange.png" class="shake"></img>');
+
+            orangeDot.width(Math.random() * (22 - 10) + 10);
+
+            body = $('body');
+
+            //full width 
+            x = Math.floor((body.width() - 24) * Math.random());
+            y = Math.floor((body.height() - 24) * Math.random());
+            orangeDot.offset({left: x, top: y});
+
+            body.append(orangeDot);
+
+        }
+        if (Math.random() < 0.04) {
+            blueDot = $('<img src="blue.png"></img>');
+
+            blueDot.width(Math.random() * (22 - 10) + 10);
+
+            body = $('body');
+            x = Math.floor((body.width() - 24) * Math.random());
+            y = Math.floor((body.height() - 24) * Math.random());
+            blueDot.offset({left: x, top: y});
+
+            body.append(blueDot);
+
+        }
+        if (Math.random() < 0.05) {
+            
+            greyDot = $('<img src="grey.png"></img>');
+
+            greyDot.width(Math.random() * (22 - 10) + 10);
+
+            body = $('body'); 
+
+            x = Math.floor((body.width() - 24) * Math.random());
+            y = Math.floor((body.height() - 24) * Math.random());
+            greyDot.offset({left: x, top: y});
+
+            body.append(greyDot);
+
+        }
+        
+    }
+
+    setInterval(updateDots, 25);
+
+    //interval;
+
+    // setTimeout(clearInterval(interval),30);
+    
+    
+    
+});
