68d7833e5ff6d6639f5a211efbdef631fe2b2857
cgjohn <cgjohn@umich.edu>
2017-01-21 16:46:15 -0500

    php

--- sentinel.php
+++ sentinel.php
@@ -1,29 +1,17 @@
-<?php 
-define('DB_NAME', 'sentinal');
-define('DB_USER', 'root');
-define('DB_PASSWORD', "");
-define('DB_HOST', 'localhost');
-
-$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-
-if (!$link) {
-	die('could not connect: '.mysql_error());
+<?php
+ 
+ 
+function Connect()
+{
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $dbname = "sentinel";
+ 
+ // Create connection
+ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
+ 
+ return $conn;
 }
-
-$db_selected = mysql_select_db(DB_NAME, $link);
-
-if(!$db_selected) {
-	die('cant use '.DB_NAME. ': '.mysql_error());
-}
-
-echo 'connected';
-
-$value = $_POST['emailIn'];
-
-$sql = "INSERT INTO sentinel (emailIn) VALUES ('$value')";
-
-if (!msql_query($sql)) {
-	die('Error: '.mysql_error())
-}
- mysql_close();
+ 
 ?>
